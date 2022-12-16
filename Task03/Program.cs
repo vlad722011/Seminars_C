38f3b0b9248e3b96014c800fa3e09b154de8c5b2
vladislav_bursaev <vlad722011@yandex.ru>
2022-12-16 18:35:35 +0700

    Added Task03 - изменилимя переменной на более правильное

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -4,12 +4,12 @@
 // 5 -> Пятница
 
 Console.WriteLine("Введите число - номер дня недели: ");
-string day = Console.ReadLine();
-if (day == "1") Console.WriteLine("Понедельник");
-else if (day == "2") Console.WriteLine("Вторник");
-else if (day == "3") Console.WriteLine("Среда");
-else if (day == "4") Console.WriteLine("Четверг");
-else if (day == "5") Console.WriteLine("Пятница");
-else if (day == "6") Console.WriteLine("Суббота");
-else if (day == "7") Console.WriteLine("Воскресенье");
+string numberOfTheDay = Console.ReadLine();
+if (numberOfTheDay == "1") Console.WriteLine("Понедельник");
+else if (numberOfTheDay == "2") Console.WriteLine("Вторник");
+else if (numberOfTheDay == "3") Console.WriteLine("Среда");
+else if (numberOfTheDay == "4") Console.WriteLine("Четверг");
+else if (numberOfTheDay == "5") Console.WriteLine("Пятница");
+else if (numberOfTheDay == "6") Console.WriteLine("Суббота");
+else if (numberOfTheDay == "7") Console.WriteLine("Воскресенье");
 else Console.WriteLine("Нет такого дня недели, либо Вы неккорректно ввели данные."); 
