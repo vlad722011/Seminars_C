193b80037c8b12fe235fb9929e8cd62d626c7db5
vladislav_bursaev <vlad722011@yandex.ru>
2022-12-16 23:40:53 +0700

    Added Task08. Решена задача №8

---
+++ Task08/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 8: Напишите программу, которая на вход
+// принимает число (N), а на выходе показывает все чётные
+// числа от 1 до N.
+
+
+Console.Write("Введите число: ");
+int finiteNumber = Convert.ToInt32(Console.ReadLine());
+
+int remainderOfTheDivision = finiteNumber % 2;
+
+if (remainderOfTheDivision == 0) finiteNumber = finiteNumber;
+else finiteNumber = finiteNumber - 1;
+
+int initialNumber = 2;
+while(initialNumber <= finiteNumber)
+{
+    Console.Write(initialNumber + " ");
+    initialNumber +=2;
+}
