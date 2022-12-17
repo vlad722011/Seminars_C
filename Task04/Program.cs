22e049854cfa953cc452b182e2ea8ec022f7a2e8
vladislav_bursaev <vlad722011@yandex.ru>
2022-12-17 09:53:29 +0700

    оптимизировано решение задачи 4

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -7,15 +7,7 @@ Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите третье число: ");
 int number3 = Convert.ToInt32(Console.ReadLine());
-
 int max = number1;
-
-if (max > number2)  max = max;
-else if (number2 >= max)  max = number2;
-else;
-
-if (max > number3)  max = max;
-else  if (number3 >= max)  max = number3;
-else;
-
+if (number2 >= max)  max = number2;
+if (number3 >= max)  max = number3;
 Console.WriteLine($"Максимальное из введенных чисел равно {max}");
