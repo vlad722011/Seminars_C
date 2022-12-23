917df6146bf3c67edda19474ead9aac9129c6a58
vladislav_bursaev <vlad722011@yandex.ru>
2022-12-23 21:06:26 +0700

    added task22_1 в папке задача 22 с выводом результата в виде таблицы

---
+++ Task22_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает на выходе
+// таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 1 -> 1, 4.
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Таблица квадратов чисел от 1 до {number}:");
+
+if (number <= 0) Console.WriteLine("Вы ввели не натуральное число.");
+else 
+{
+    int count = 1;
+    while (count <= number)
+    {
+        double numbersSquares = NumbersSquares(count);
+        Console.WriteLine( count + "  " + numbersSquares);
+        count++;
+    }
+}
+
+double NumbersSquares(int num)
+{
+    double squares;
+    return squares = Math.Pow(num, 2);
+}
