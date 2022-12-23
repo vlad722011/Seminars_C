ce3701aaffeb7cabf2d4096b13bdee7c07493047
vladislav_bursaev <vlad722011@yandex.ru>
2022-12-23 21:35:57 +0700

    сделал более красивый вывод

--- Task22_2/Program.cs
+++ Task22_2/Program.cs
@@ -3,23 +3,23 @@
 // 5 -> 1, 4, 9, 16, 25.
 // 1 -> 1, 4.
 
-Console.WriteLine("Введите натуральное число: ");
+Console.Write("Введите натуральное число:  ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"Таблица квадратов чисел от 1 до {number}:");
-
-if (number > 0)
+if (number <= 0) Console.WriteLine("Вы ввели не натуральное число.");
+else
 {
+    Console.WriteLine($"Таблица квадратов чисел от 1 до {number}:");
     TableSquare(number);
 }
-else Console.WriteLine("Вы ввели не натуральное число.");
+
 
 void TableSquare(int num)
 {
     int count = 1;
-    while(count <= num)
+    while (count <= num)
     {
-        Console.WriteLine($"{count}" + "   " + Math.Pow(count, 2) );
-        count ++;
+        Console.WriteLine($"{count}" + "   " + Math.Pow(count, 2));
+        count++;
     }
 }
