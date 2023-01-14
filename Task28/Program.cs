deb9d40ec18ba6105eba963c4be0eafe47fca535
vladislav_bursaev <vlad722011@yandex.ru>
2023-01-14 13:08:12 +0700

    added Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int factorial = FactorialOfNumbers(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {factorial}");
+
+int FactorialOfNumbers(int num)
+{
+    int factorial = 1;
+    for (int i = 1; i <= num; i++ )
+    {
+        factorial *= i;
+    }
+return factorial;
+}
+
