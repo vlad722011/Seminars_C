ba21820229a2f5a486f2790795b167f8ff5f0f9d
vladislav_bursaev <vlad722011@yandex.ru>
2023-01-17 20:50:36 +0700

    added task36

---
+++ Task36/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// Примеры:
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите число, длину будущего входящего массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число, начало диапазона случайных чисел, которыми будет заполнен массив: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число, конец диапазона случайных чисел, которыми будет заполнен массив: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArrayRndInt(size, min, max);
+Console.Write("Входящий массив: -> ");
+PrintArray(array);
+int sumOfElementsWithOddIndexes = SumOfElementsWithOddIndexes(array);
+Console.Write($"Сумма элементов массива на нечетных позициях = {sumOfElementsWithOddIndexes}.");
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int SumOfElementsWithOddIndexes(int [] arr)
+{
+    int sum  = 0;
+     for (int i = 0; i < arr.Length; i++)
+    {
+        if(i % 2 == 1) sum += arr[i]; 
+    }
+    return sum;
+}
