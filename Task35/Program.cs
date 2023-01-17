ba2a43945411b8d4395cfb14594aa0179436bc17
vladislav_bursaev <vlad722011@yandex.ru>
2023-01-17 17:17:52 +0700

    added task35

---
+++ Task35/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 35. Задайте одномерный массив и 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в отрезке [10, 99].
+// Пример дан для массива из 5 элементов:
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+// Задачу сделать для массива из 123 элементов.
+
+int size = 123; // количесвто элементов будущего массива.
+Console.WriteLine("Введите число, начало диапазона случайных чисел, которыми будет заполнен массив: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число, конец диапазона случайных чисел, которыми будет заполнен массив: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArrayRndInt(size, min, max);
+Console.Write("Входящий массив: -> ");
+PrintArray(array);
+int countElements = GetCountOfElementsFromARange(array);
+Console.Write($"Количество элементов массива из диапазона [10, 99]  = {countElements}");
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int GetCountOfElementsFromARange(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99) count += 1;
+    }
+    return count;
+}
