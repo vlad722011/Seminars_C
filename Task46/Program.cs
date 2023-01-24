5eaef95919ae4b35cc66115c52777a66e96192ff
vladislav_bursaev <vlad722011@yandex.ru>
2023-01-24 15:42:12 +0700

    added task46 and task48

---
+++ Task46/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 46. Задайте двумерный массив, размером M x N,
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1, 4, 8, 19
+// 5, -2, 33, -2
+// 77, 3, 8, 1
+
+int rows = 3;
+int columns = 4;
+int min = -99;
+int max = 99;
+
+int[,] array2D = CreateMatrixRndInt(rows, columns, min, max);
+PrintMatrix(array2D);
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = rnd.Next(min, max + 1);
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+
+}
+
+
---
+++ Task48/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 48. Задайте двумерный массив размера m на n,
+// где каждый элемент в массиве находится по формуле
+// Amn = m + n.
+// Выведете полученный массив на экран.
+// m = 3, n = 4.
+// [0, 1, 2, 3]
+// [1, 2, 3, 4]
+// [2, 3, 4, 5].
+
+int rows = 3;
+int columns = 4;
+int[,] array2D = CreateMatrixSumIndex(rows, columns);
+PrintMatrix(array2D);
+
+int[,] CreateMatrixSumIndex(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = i + j;
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4}   ");
+        }
+        Console.WriteLine("]");
+    }
+
+}
+
