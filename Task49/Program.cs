23eb386f37535389b67f518efbf687086d8fa074
vladislav_bursaev <vlad722011@yandex.ru>
2023-01-24 16:17:29 +0700

    added task49

---
+++ Task49/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их квадраты.
+// Например, изначально массив выглядел вот так:
+
+//  [ 1    4    7    2 ]
+//  [ 5    9    2    3 ]
+//  [ 8    4    2    4 ]
+
+// Новый массив будет выглядеть вот так:
+
+//  [ 1    4    49    2 ]
+//  [ 5    9     2    3 ]
+//  [ 64   4     4    4 ]
+
+Console.WriteLine("Введите число строк, будущего двумерного массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число столбцов, будущего двумерного массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число, начало диапазона случайных чисел, которыми будет заполнен массив: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число, конец диапазона случайных чисел, которыми будет заполнен массив: ");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+
+int[,] array2D = CreateMatrixRndInt(rows, columns, min, max);
+PrintMatrix(array2D);
+Console.WriteLine();
+array2D = ChangeMatrix(array2D);
+PrintMatrix(array2D);
+Console.WriteLine();
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = rnd.Next(min, max + 1);
+    }
+    return matrix;
+}
+
+int[,] ChangeMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0) matrix[i, j] = matrix[i, j] * matrix[i, j];
+        }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4}   ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
