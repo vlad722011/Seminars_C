aaca681cd52d814ee0637c465ac8f0fb10f2fd75
vladislav_bursaev <vlad722011@yandex.ru>
2023-01-31 00:08:58 +0700

    задачу 62 доделал под всевозможные варианты (квадраты, прямоугольники) разные размеры

--- Task62/Program.cs
+++ Task62/Program.cs
@@ -6,44 +6,100 @@
 // [ 10  09  08  07 ]
 
 int number = 1; //стартовое значение первого элемента матрицы
-int width = 10; // ширина рисуемого прямоугольника
-int height = 10; // высота рисуемого квадрата
-int rows = 10; // количество строк исходной матрицы
-int columns = 10; // количество столбцов исходной матрицы
+int width = 4; // ширина рисуемого прямоугольника
+int height = 8; // высота рисуемого квадрата
+int rows = 12; // количество строк исходной матрицы
+int columns = 12; // количество столбцов исходной матрицы
 int rowFirst = 0;  // индекс строки стартового элемента
 int colFirst = 0; //индекс столбца стартогвого элемента
 
-
 int counter = Counter(width, height);
+Console.WriteLine($"{counter}");
 int[,] matrix = new int[rows, columns];
 int[] arrStart = new int[5] { number, width, height, rowFirst, colFirst };
-
 for (int i = 0; i < counter; i++)
 {
-    matrix = DrawSquare(arrStart, matrix);
-    arrStart = TempArray(arrStart);   
+    if (height % 2 == 0 && width % 2 == 0 && height == width)
+    {
+        matrix = DrawSquare(arrStart, matrix);
+        arrStart = TempArray(arrStart);
+    }
+    if (height % 2 == 0 && width % 2 == 0 && height > width)
+    {
+        if (i != counter - 1)
+        {
+            matrix = DrawSquare(arrStart, matrix);
+            arrStart = TempArray(arrStart);
+        }
+        else if (i == counter - 1)
+        {
+            DrawSquare1(arrStart, matrix);
+        }
+    }
+    else if (height % 2 == 0 && width % 2 == 0 && height < width)
+    {
+        if (i != counter - 1)
+        {
+            matrix = DrawSquare(arrStart, matrix);
+            arrStart = TempArray(arrStart);
+        }
+        if (i == counter - 1)
+        {
+            matrix = DrawSquareLastRow(arrStart, matrix);
+            matrix = DrawSquareLastRowDown(arrStart, matrix);
+        }
+    }
+    else if (height % 2 == 1 && width % 2 == 0)
+    {
+        if (i != counter - 1)
+        {
+            matrix = DrawSquare(arrStart, matrix);
+            arrStart = TempArray(arrStart);
+        }
+        if (i == counter - 1)
+        {
+            matrix = DrawSquare(arrStart, matrix);
+            arrStart = TempArray(arrStart);
+            matrix = DrawSquareLastRow(arrStart, matrix);
+        }
+    }
+    else if (height % 2 == 0 && width % 2 == 1)
+    {
+        if (i != counter - 1)
+        {
+            matrix = DrawSquare(arrStart, matrix);
+            arrStart = TempArray(arrStart);
+        }
+        if (i == counter - 1)
+        {
+            matrix = DrawSquareLastCol(arrStart, matrix);
+        }
+    }
+    else if (height % 2 == 1 && width % 2 == 1)
+        if (i != counter - 1)
+        {
+            matrix = DrawSquare(arrStart, matrix);
+            arrStart = TempArray(arrStart);
+        }
+    if (i == counter - 1)
+    {
+        if (height % 2 == 1 && width % 2 == 1 && width >= height)
+        {
+            matrix = DrawSquareLastRow(arrStart, matrix);
+        }
+        else if (height % 2 == 1 && width % 2 == 1 && width < height)
+        {
+            matrix = DrawSquareLastCol(arrStart, matrix);
+        }
+    }
 }
-
 Console.WriteLine("Заполненная матрица: -> ");
 Console.WriteLine();
 PrintMatrix(matrix);
 Console.WriteLine();
 
-
-int[] TempArray(int[] arr)
-{
-    number = number + (2 * width) + (2 * (height - 2));
-    width = width - 2;
-    height = height - 2;
-    colFirst += 1;
-    rowFirst += 1;
-    arr = new int[5] { number, width, height, rowFirst, colFirst };
-    return arr;
-}
-
 int[,] DrawSquare(int[] arr, int[,] matrix)
 {
-    
     int num = arr[0];
     int indexRow = arr[3];
     int indexCol = arr[4];
@@ -69,9 +125,76 @@ int[,] DrawSquare(int[] arr, int[,] matrix)
         num += 1;
     }
     // рисуем левый край квадрата
-
     indexCol = arr[4];
     for (indexRow = arr[3] + arr[2] - 2; indexRow > arr[3]; indexRow--)
+    {
+        matrix[indexRow, indexCol] = num;
+        num += 1;
+    }
+    return matrix;
+}
+
+int[,] DrawSquareLastRow(int[] arr, int[,] matrix)
+{
+    int num = arr[0];
+    int indexRow = arr[3];
+    int indexCol = arr[4];
+    for (int j = indexCol; j < (indexCol + arr[1]); j++)
+    {
+        matrix[indexRow, j] = num;
+        num += 1;
+    }
+    return matrix;
+}
+
+int[,] DrawSquareLastCol(int[] arr, int[,] matrix)
+{
+    int num = arr[0];
+    int indexRow = arr[3];
+    int indexCol = arr[4];
+    for (indexRow = arr[3]; indexRow < arr[3] + arr[2]; indexRow++)
+    {
+        matrix[indexRow, indexCol] = num;
+        num += 1;
+    }
+    return matrix;
+}
+
+int[,] DrawSquareLastRowDown(int[] arr, int[,] matrix)
+{
+    int num = arr[0] + arr[1];
+    int indexRow = arr[3] + 1;
+    int indexCol = arr[4] + arr[1] - 1;
+    for (int k = indexCol; k >= arr[4]; k--)
+    {
+        matrix[indexRow, k] = num;
+        num += 1;
+    }
+    return matrix;
+}
+
+int[,] DrawSquare1(int[] arr, int[,] matrix)
+{
+    int num = arr[0];
+    int indexRow = arr[3];
+    int indexCol = arr[4];
+    // рисуем верх квадрата
+    for (int j = indexCol; j < (indexCol + arr[1]); j++)
+    {
+        matrix[indexRow, j] = num;
+        num += 1;
+    }
+    // рисуем правый край квадрата
+    indexCol = arr[4] + arr[1] - 1;
+    for (indexRow = arr[3] + 1; indexRow < arr[3] + arr[2]; indexRow++)
+    {
+        matrix[indexRow, indexCol] = num;
+        num += 1;
+    }
+    //рисуем левый край
+    num = arr[0] + arr[2] + 1;
+    indexCol = arr[4];
+    for (indexRow = arr[3] + arr[2] - 1; indexRow > arr[3]; indexRow--)
     {
         matrix[indexRow, indexCol] = num;
         num += 1;
@@ -105,4 +228,15 @@ int Counter(int width, int height)
     if (countHeight < countWidth) count = countHeight;
     else count = countWidth;
     return count;
+}
+
+int[] TempArray(int[] arr)
+{
+    number = number + (2 * width) + (2 * (height - 2));
+    width = width - 2;
+    height = height - 2;
+    colFirst += 1;
+    rowFirst += 1;
+    arr = new int[5] { number, width, height, rowFirst, colFirst };
+    return arr;
 }
--- TaskTEST/Program.cs
+++ TaskTEST/Program.cs
@@ -1,43 +1,203 @@
-﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
-// которая упорядочит по убыванию элементы каждой строки двумерного массива.
-// Например, задан массив:
-// [1  4  7  2]
-// [5  9  2  3]
-// [8  4  2  4]
-// В итоге получается вот такой массив:
-// [7  4  2  1]
-// [9  5  3  2]
-// [8  4  4  2]
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// [ 01  02  03  04 ]
+// [ 12  13  14  05 ]
+// [ 11  16  15  06 ]
+// [ 10  09  08  07 ]
 
-Console.WriteLine("Введите число строк, будущего двумерного массива: ");
-int rows = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число столбцов, будущего двумерного массива: ");
-int columns = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число, начало диапазона случайных чисел, которыми будет заполнен массив: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число, конец диапазона случайных чисел, которыми будет заполнен массив: ");
-int max = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
+int number = 1; //стартовое значение первого элемента матрицы
+int width = 4; // ширина рисуемого прямоугольника
+int height = 8; // высота рисуемого квадрата
+int rows = 12; // количество строк исходной матрицы
+int columns = 12; // количество столбцов исходной матрицы
+int rowFirst = 0;  // индекс строки стартового элемента
+int colFirst = 0; //индекс столбца стартогвого элемента
 
-int[,] matrix = CreateMatrixRndInt(rows, columns, min, max);
-Console.WriteLine("Входящий массив: - > ");
+int counter = Counter(width, height);
+Console.WriteLine($"{counter}");
+int[,] matrix = new int[rows, columns];
+int[] arrStart = new int[5] { number, width, height, rowFirst, colFirst };
+for (int i = 0; i < counter; i++)
+{
+    if (height % 2 == 0 && width % 2 == 0 && height == width)
+    {
+        matrix = DrawSquare(arrStart, matrix);
+        arrStart = TempArray(arrStart);
+    }
+    if (height % 2 == 0 && width % 2 == 0 && height > width)
+    {
+        if (i != counter - 1)
+        {
+            matrix = DrawSquare(arrStart, matrix);
+            arrStart = TempArray(arrStart);
+        }
+        else if (i == counter - 1)
+        {
+            DrawSquare1(arrStart, matrix);
+        }
+    }
+    else if (height % 2 == 0 && width % 2 == 0 && height < width)
+    {
+        if (i != counter - 1)
+        {
+            matrix = DrawSquare(arrStart, matrix);
+            arrStart = TempArray(arrStart);
+        }
+        if (i == counter - 1)
+        {
+            matrix = DrawSquareLastRow(arrStart, matrix);
+            matrix = DrawSquareLastRowDown(arrStart, matrix);
+        }
+    }
+    else if (height % 2 == 1 && width % 2 == 0)
+    {
+        if (i != counter - 1)
+        {
+            matrix = DrawSquare(arrStart, matrix);
+            arrStart = TempArray(arrStart);
+        }
+        if (i == counter - 1)
+        {
+            matrix = DrawSquare(arrStart, matrix);
+            arrStart = TempArray(arrStart);
+            matrix = DrawSquareLastRow(arrStart, matrix);
+        }
+    }
+    else if (height % 2 == 0 && width % 2 == 1)
+    {
+        if (i != counter - 1)
+        {
+            matrix = DrawSquare(arrStart, matrix);
+            arrStart = TempArray(arrStart);
+        }
+        if (i == counter - 1)
+        {
+            matrix = DrawSquareLastCol(arrStart, matrix);
+        }
+    }
+    else if (height % 2 == 1 && width % 2 == 1)
+        if (i != counter - 1)
+        {
+            matrix = DrawSquare(arrStart, matrix);
+            arrStart = TempArray(arrStart);
+        }
+    if (i == counter - 1)
+    {
+        if (height % 2 == 1 && width % 2 == 1 && width >= height)
+        {
+            matrix = DrawSquareLastRow(arrStart, matrix);
+        }
+        else if (height % 2 == 1 && width % 2 == 1 && width < height)
+        {
+            matrix = DrawSquareLastCol(arrStart, matrix);
+        }
+    }
+}
+Console.WriteLine("Заполненная матрица: -> ");
 Console.WriteLine();
 PrintMatrix(matrix);
 Console.WriteLine();
-Console.WriteLine("Полученный массив с отсортированными по убыванию строками: - > ");
-int[,] newMatrix = GetMatrixWithSortRows(matrix);
-PrintMatrix(newMatrix);
-Console.WriteLine();
 
-
-int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+int[,] DrawSquare(int[] arr, int[,] matrix)
 {
-    int[,] matrix = new int[rows, columns];
-    Random rnd = new Random();
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    int num = arr[0];
+    int indexRow = arr[3];
+    int indexCol = arr[4];
+    // рисуем верх квадрата
+    for (int j = indexCol; j < (indexCol + arr[1]); j++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-            matrix[i, j] = rnd.Next(min, max + 1);
+        matrix[indexRow, j] = num;
+        num += 1;
+    }
+    // рисуем правый край квадрата
+    indexCol = arr[4] + arr[1] - 1;
+    for (indexRow = arr[3] + 1; indexRow < arr[3] + arr[2] - 1; indexRow++)
+    {
+        matrix[indexRow, indexCol] = num;
+        num += 1;
+    }
+    // рисуем низ квадрата
+    indexCol = arr[4] + arr[1] - 1;
+    indexRow = arr[3] + arr[2] - 1;
+    for (int k = indexCol; k >= arr[4]; k--)
+    {
+        matrix[indexRow, k] = num;
+        num += 1;
+    }
+    // рисуем левый край квадрата
+    indexCol = arr[4];
+    for (indexRow = arr[3] + arr[2] - 2; indexRow > arr[3]; indexRow--)
+    {
+        matrix[indexRow, indexCol] = num;
+        num += 1;
+    }
+    return matrix;
+}
+
+int[,] DrawSquareLastRow(int[] arr, int[,] matrix)
+{
+    int num = arr[0];
+    int indexRow = arr[3];
+    int indexCol = arr[4];
+    for (int j = indexCol; j < (indexCol + arr[1]); j++)
+    {
+        matrix[indexRow, j] = num;
+        num += 1;
+    }
+    return matrix;
+}
+
+int[,] DrawSquareLastCol(int[] arr, int[,] matrix)
+{
+    int num = arr[0];
+    int indexRow = arr[3];
+    int indexCol = arr[4];
+    for (indexRow = arr[3]; indexRow < arr[3] + arr[2]; indexRow++)
+    {
+        matrix[indexRow, indexCol] = num;
+        num += 1;
+    }
+    return matrix;
+}
+
+int[,] DrawSquareLastRowDown(int[] arr, int[,] matrix)
+{
+    int num = arr[0] + arr[1];
+    int indexRow = arr[3] + 1;
+    int indexCol = arr[4] + arr[1] - 1;
+    for (int k = indexCol; k >= arr[4]; k--)
+    {
+        matrix[indexRow, k] = num;
+        num += 1;
+    }
+    return matrix;
+}
+
+int[,] DrawSquare1(int[] arr, int[,] matrix)
+{
+    int num = arr[0];
+    int indexRow = arr[3];
+    int indexCol = arr[4];
+    // рисуем верх квадрата
+    for (int j = indexCol; j < (indexCol + arr[1]); j++)
+    {
+        matrix[indexRow, j] = num;
+        num += 1;
+    }
+    // рисуем правый край квадрата
+    indexCol = arr[4] + arr[1] - 1;
+    for (indexRow = arr[3] + 1; indexRow < arr[3] + arr[2]; indexRow++)
+    {
+        matrix[indexRow, indexCol] = num;
+        num += 1;
+    }
+    //рисуем левый край
+    num = arr[0] + arr[2] + 1;
+    indexCol = arr[4];
+    for (indexRow = arr[3] + arr[2] - 1; indexRow > arr[3]; indexRow--)
+    {
+        matrix[indexRow, indexCol] = num;
+        num += 1;
     }
     return matrix;
 }
@@ -56,52 +216,27 @@ void PrintMatrix(int[,] matrix)
     }
 }
 
-int[] SplitInToRows(int[,] arr2D, int indexRow)
+int Counter(int width, int height)
 {
-    int[] arr = new int[arr2D.GetLength(1)];
-    for (int j = 0; j < arr2D.GetLength(1); j++)
-    {
-        arr[j] = arr2D[indexRow, j];
-    }
-    return arr;
-}
-
-int[] SortArray(int[] arr)
-{
+    int countWidth = 0;
+    if (width % 2 == 0) countWidth = width / 2;
+    else countWidth = width / 2 + 1;
+    int countHeight = 0;
+    if (width % 2 == 0) countHeight = height / 2;
+    else countHeight = height / 2 + 1;
     int count = 0;
-    int temp = 0;
-    int index = 0;
-    int maximum = Int32.MinValue;
-    while (count < arr.Length)
-    {
-        for (int i = count; i < arr.Length; i++)
-        {
-            if (arr[i] > maximum)
-            {
-                maximum = arr[i];
-                index = i;
-            }
-        }
-        temp = arr[count];
-        arr[count] = arr[index];
-        arr[index] = temp;
-        count += 1;
-        maximum = Int32.MinValue;
-    }
-    return arr;
+    if (countHeight < countWidth) count = countHeight;
+    else count = countWidth;
+    return count;
 }
 
-int[,] GetMatrixWithSortRows(int[,] arr2D)
+int[] TempArray(int[] arr)
 {
-    int[,] newArr2D = new int[arr2D.GetLength(0), arr2D.GetLength(1)];
-    for (int i = 0; i < newArr2D.GetLength(0); i++)
-    {
-        for (int j = 0; j < arr2D.GetLength(1); j++)
-        {
-            int[] arr = SplitInToRows(arr2D, i);
-            SortArray(arr);
-            newArr2D[i, j] = arr[j];
-        }
-    }
-    return newArr2D;
+    number = number + (2 * width) + (2 * (height - 2));
+    width = width - 2;
+    height = height - 2;
+    colFirst += 1;
+    rowFirst += 1;
+    arr = new int[5] { number, width, height, rowFirst, colFirst };
+    return arr;
 }
