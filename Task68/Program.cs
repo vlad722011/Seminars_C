f102499e3e2583f95fe93141782d01ca476f7692
vladislav_bursaev <vlad722011@yandex.ru>
2023-01-31 22:06:55 +0700

    added task68

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -2,3 +2,23 @@
 // Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Введите положительное число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите положительное число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int result = Akkerman(numberM, numberN);
+if (numberM >= 0 && numberN >= 0) Console.WriteLine($"m = {numberM}, n = {numberN} -> A (m, n) = {result}");
+else Console.WriteLine("Не все числа положительные. Введите корректные данные");
+
+int Akkerman(int numM, int numN)
+{
+    if (numM == 0) return numN + 1;
+    else if((numM > 0) && (numN == 0)) return Akkerman (numM -1, 1);
+    else return Akkerman (numM -1, Akkerman(numM, numN -1));
+}
+
+
+
+
