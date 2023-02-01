7af28186ab7877b92f551af687ed0f93916ff201
vladislav_bursaev <vlad722011@yandex.ru>
2023-02-01 09:56:56 +0700

    поправил недочет в 66 задаче. метод вне блока else

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -25,11 +25,12 @@ else
     int sum = SumNaturalElementRange(number1, number2);
     Console.WriteLine($"Сумма натуральных элементов в заданном диапазоне равна: {sum}");
     Console.WriteLine();
+    SumNaturalElementRange(number1, number2);
+}
 
-    int SumNaturalElementRange(int num1, int num2)
-    {
-        if (num1 == 0 && num2 == 0) return num1;
-        if (num1 == num2) return num1;
-        return num1 + SumNaturalElementRange(num1 + 1, num2);
-    }
+int SumNaturalElementRange(int num1, int num2)
+{
+    if (num1 == 0 && num2 == 0) return num1;
+    if (num1 == num2) return num1;
+    return num1 + SumNaturalElementRange(num1 + 1, num2);
 }
